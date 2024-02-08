715c4cdf47f576bc548b3c9ee2facb7e6f388a3e
Vishal Maharathy <vishalmaharathy76@gmail.com>
2024-02-09 01:47:46 +0530

    todo app modified with advancements

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,61 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Todo App</h1>
+      <h1>Todos App</h1>
       <Todo />
     </>
   )
 }
 
+interface TodoContainer{
+  title: string,
+  description: string,
+  done: boolean
+}
+
 function Todo() {
-  const [todos, setTodos] = useState<string[]>([])
-  const [todo, setTodo] = useState<string>('')
-  function handleAddTodo() {
-    setTodos([...todos, todo])
-    setTodo('')
+  const [todo, setTodo] = useState<TodoContainer[]>([])
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+
+  const addTodo = () => {
+    setTodo([
+      ...todo,
+      {
+        title,
+        description,
+        done: false
+      }
+    ])
+    setTitle('')
+    setDescription('')
   }
+
+  const markAsDone = (index: number) => {
+    const newTodo = [...todo]
+    newTodo[index].done = !newTodo[index].done
+    setTodo(newTodo)
+  }
+
+  const deleteTodo = (index: number) => {
+    const newTodo = [...todo]
+    newTodo.splice(index, 1)
+    setTodo(newTodo)
+  }
+
   return (
     <div>
-      <input value={todo} onChange={(e) => setTodo(e.target.value)} />
-      <button onClick={handleAddTodo}>Add</button>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title" />
+      <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Description" />
+      <button onClick={addTodo}>Add</button>
       <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
+        {todo.map((item, index) => (
+          <li key={index}>
+            {!item.done?<h3>{item.title}</h3>:<h3 style={{textDecoration: 'line-through'}}>{item.title}</h3>}
+            <p>{item.description}</p>
+            <button onClick={() => markAsDone(index)}>Done</button>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
