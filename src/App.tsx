9af1850a87b6629c6fea42ba98041a89a5599d97
Vishal Maharathy <vishalmaharathy76@gmail.com>
2024-02-09 01:40:09 +0530

    typescript todo app

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,31 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  function handleClick() {
-    setCount(count+1)
-  }
   return (
     <>
-      <h1>{count}</h1>
-      <button onClick={handleClick}>Count</button>
+      <h1>Todo App</h1>
+      <Todo />
     </>
   )
 }
 
+function Todo() {
+  const [todos, setTodos] = useState<string[]>([])
+  const [todo, setTodo] = useState<string>('')
+  function handleAddTodo() {
+    setTodos([...todos, todo])
+    setTodo('')
+  }
+  return (
+    <div>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={handleAddTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 export default App
